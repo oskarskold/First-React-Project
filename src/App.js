11a2508a0e7c40731704e25be0a6c4878ba28b8f
Oskar <Oskar.skold4@hotmail.com>
2021-11-24 11:40:34 +0100

    First React Project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Hörlurar",
+      amount: 999,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "Glass", amount: 20, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Skor",
+      amount: 699,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Datorskärm",
+      amount: 1450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
