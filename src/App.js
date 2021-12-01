4ec8207348585bc0d6c731485e3a496034194853
Oskar <Oskar.skold4@hotmail.com>
2021-12-01 09:21:36 +0100

    Added map and filter so we can add new list items and filter by year

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Hörlurar",
+    amount: 999,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "Glass", amount: 20, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Skor",
+    amount: 699,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Datorskärm",
+    amount: 1450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Hörlurar",
-      amount: 999,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "Glass", amount: 20, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Skor",
-      amount: 699,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Datorskärm",
-      amount: 1450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
